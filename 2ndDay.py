2a5a8e3c0114272f492d4b1f597f1cea922af004
Rujan Shrestha <rujans57@gmail.com>
2025-01-02 08:36:18 +0545

    Modified comments

--- 2ndDay.py
+++ 2ndDay.py
@@ -1,80 +1,80 @@
-# #Exception Handling
-# try:
-#     a=5
-#     b=7
-#     print(a+b)
-# except:
-#     print("There was an error")
-# finally:
-#     print("Code run sucessfully")
+#Exception Handling
+try:
+    a=5
+    b=7
+    print(a+b)
+except:
+    print("There was an error")
+finally:
+    print("Code run sucessfully")
 
-# #Raise Exception
-# try:
-#     a=5
-#     b=7
-#     print(a+b)
-#     raise Exception("qwerty")
-# except:
-#     print("There was an error")
-# finally:
-#     print("Code run sucessfully")
+#Raise Exception
+try:
+    a=5
+    b=7
+    print(a+b)
+    raise Exception("qwerty")
+except:
+    print("There was an error")
+finally:
+    print("Code run sucessfully")
 
-# #
-# try:
-#     a = (1,2,3,'hello')
-#     for i in a:
+#
+try:
+    a = (1,2,3,'hello')
+    for i in a:
         
-#         print(i)
-#         print(isinstance(i,int))
-#         if(isinstance(i,int)):
-#             continue
-#         else:
-#             raise Exception()
-# except:
-#     print("There was an error")
-# finally:
-#     print("Code run sucessfully")
+        print(i)
+        print(isinstance(i,int))
+        if(isinstance(i,int)):
+            continue
+        else:
+            raise Exception()
+except:
+    print("There was an error")
+finally:
+    print("Code run sucessfully")
 
-# #class
-# class myClass:
-#     x = 5
+#class
+class myClass:
+    x = 5
 
-# #object
-# c1 = myClass
+#object
+c1 = myClass
 
 # def __init__ Funciton
 # def __str__ Function
 
-# #constructor
-# class Person:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+#constructor
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
     
-#     def __str__(self):
-#         return f"{self.name}({self.age})"
+    def __str__(self):
+        return f"{self.name}({self.age})"
 
-# p1 = Person("Azay", 21)
+p1 = Person("Azay", 21)
 
-# print(p1)
+print(p1)
 
-# #
-# class Person:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+#
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
     
-#     def __str__(self):
-#         return f"{self.name}({self.age})"
+    def __str__(self):
+        return f"{self.name}({self.age})"
 
-# p1 = Person("Azay", 21)
+p1 = Person("Azay", 21)
 
-# print(p1.name, p1.age)
+print(p1.name, p1.age)
 
-# #Math function
-# import math
+#Math function
+import math
 
-# print(math.pi)
+print(math.pi)
 
 import json
 
